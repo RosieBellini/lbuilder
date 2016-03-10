31832e84f39db3eac8e728198c6af822f28160a9
b5056853 <b5056853@8242c0c4-60f8-46ac-9e85-01df940c0d57>
2016-03-10 14:48:13 +0000

    added boxterminator branch. this branch has a new hierarchy (again) and some source code changes so that the game can be compiled and run as a package. had to disable open menu to get around compilation errors - see if you can fix this josh

---
+++ src/main/java/team1/BoxTerm.java
@@ -0,0 +1,211 @@
+package team1;
+
+import java.util.*;
+import java.io.*;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/** Box Terminator main method. This class handles importing the level, drawing
+ * the game screen and interpreting key presses.
+ *
+ * TODO:    -Fix freeze when clicking on the level
+ *          -Draw the level with something more appropriate - JavaFX or Graphics
+ *          2d?
+ */
+
+@SuppressWarnings("serial")
+public class BoxTerm extends JPanel {
+    private static SokobanMap map;
+    private static int xSize;
+    private static int ySize;
+    private static JTextArea textArea;
+    // SpriteMap class to display the tileset.
+    private static SpriteMap spriteMap;
+    private static JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+    private static int tileSetNo=1;
+
+    /**
+     * A constructor to initialise the key listener which allows methods to be
+     * run when key presses are detected
+     */
+    public BoxTerm() {
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                moveWorker(e);
+            }
+        });
+        setFocusable(true);
+    }
+
+    /**
+     * Interprets the contents of the "level" file and stores it as a SokobanMap
+     */
+    private static void importLevel(InputStream levelFile) {
+        if (levelFile == null) { //If Getfile is cancelled.
+            return;
+        }
+        int x = 0;
+        int y = 0;
+        xSize = 0;
+        ySize = 0;
+        /*
+         * First, get the raw data as an array of strings and use this to
+         * determine the size of the level
+         */
+        ArrayList<String> levelLines = new ArrayList<String>();
+        Scanner level = new Scanner(levelFile);
+        while (level.hasNextLine()) {
+            String line = level.nextLine();
+            if (line.length() > xSize) {
+                xSize = line.length();
+            }
+            levelLines.add(line);
+        }
+        ySize = levelLines.size();
+
+        /*
+         * Then convert the raw data into a SokobanMap using the static
+         * method charToSokobanObject from the SokobanObject class
+         */
+        map = new SokobanMap(xSize, ySize);
+        for (String line: levelLines) {
+            for (char ch: line.toCharArray()) {
+                Coordinate coord = new Coordinate(x, y);
+                SokobanObject object = SokobanObject.charToSokobanObject(ch);
+                map.put(object, coord);
+                x++;
+            }
+            x = 0;
+            y++;
+        }
+        level.close();
+        map.growGrass();
+    }
+
+    /**
+     * Runs player movement methods when keypresses are detected, then checks
+     * to see if the level has been completed. If it has, displays "YOU WON!",
+     * else redraws the level.
+     *
+     * TODO:    Only check if win conditions have been met when a box is placed
+     *          on a goal rather than every time the player moves
+     */
+    private static void moveWorker(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_W:     map.move(new Coordinate(0, -1));
+                                    break;
+            case KeyEvent.VK_S:     map.move(new Coordinate(0, 1));
+                                    break;
+            case KeyEvent.VK_A:     map.move(new Coordinate(-1, 0));
+                                    break;
+            case KeyEvent.VK_D:     map.move(new Coordinate(1, 0));
+                                    break;
+            case KeyEvent.VK_U:     map.undo(true);
+                                    break;
+            case KeyEvent.VK_R:     map.redo();
+                                    break;
+            case KeyEvent.VK_H:     map.getChanges();
+                                    break;
+            default:                return;
+        }
+        redraw();
+    }
+
+    private static void makeMenuBar(JFrame frame)
+    {
+        final int SHORTCUT_MASK =
+            Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+
+        JMenuBar menubar = new JMenuBar();
+        frame.setJMenuBar(menubar);
+
+        // create the File manu
+        JMenu fileMenu = new JMenu("File");
+        menubar.add(fileMenu);
+
+        // JMenuItem openItem = new JMenuItem("Open");
+        // openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, SHORTCUT_MASK));
+        // openItem.addActionListener(new ActionListener() {
+        // 	public void actionPerformed(ActionEvent e) {
+        // 	// GOT TO SORT THIS OUT.  Possibly put this and repeated code in main method in importLevel().
+        // 		importLevel(getFile());
+        // 		frame.remove(spriteMap);
+        // 		spriteMap = new SpriteMap(map);
+        // 		redraw();
+        // 		frame.add(spriteMap);
+        // 		frame.pack();}
+        // });
+        // fileMenu.add(openItem);
+
+        JMenuItem quitItem = new JMenuItem("Quit");
+        quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
+        quitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) { System.exit(0);; }
+        });
+        fileMenu.add(quitItem);
+
+        JMenuItem tileItem = new JMenuItem("Change Tileset");
+        tileItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, SHORTCUT_MASK));
+        tileItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tileSetNo++;
+                spriteMap.loadSprites(tileSetNo%3);; }
+        });
+        fileMenu.add(tileItem);
+    }
+
+    public static File getFile()
+    {
+        int returnVal = fileChooser.showOpenDialog(null);
+        if(returnVal != JFileChooser.APPROVE_OPTION) {
+            return null;  // cancelled
+        }
+        File selectedFile = fileChooser.getSelectedFile();
+        return selectedFile;
+    }
+
+    /**
+     * Updates the contents of the game window
+     */
+    public static void redraw() {
+        textArea.setText(map.toString());
+        textArea.append(Integer.toString(map.totalHistoryLength() - 1));
+        // New line here to redraw the spriteMap.
+        spriteMap.placeSprites();
+    }
+
+    public static void main(String[] args) {
+        // importLevel(new File("src/main/resources/level"));
+        InputStream level = BoxTerm.class.getClassLoader().getResourceAsStream("level");
+        importLevel(level);
+        BoxTerm boxterm = new BoxTerm();
+        JFrame frame = new JFrame("Box Terminator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textArea = new JTextArea(ySize + 1, xSize + 1);
+        textArea.setFont(new Font("monospaced", Font.PLAIN, 24));
+        textArea.setEditable(false);
+        spriteMap = new SpriteMap(map);
+        redraw();
+        frame.add(boxterm);
+        frame.add(spriteMap);
+        makeMenuBar(frame);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ src/main/java/team1/MapContainer.java
@@ -0,0 +1,313 @@
+package team1;
+
+import java.util.*;
+
+/**
+ * Represents a Sokoban level. "Static" objects (spaces, walls, goals) are
+ * represented in an array of SokobanObjects whereas "dynamic" objects (boxes
+ * and the player) are represented in a FixedSizeStack of SaveState objects.
+ * This separation reduces the amount of information we need to store for the
+ * undo command and solver.
+ *
+ * TODO: make the array of static objects immutable
+ */
+public class MapContainer {
+	private FixedSizeStack<SaveState> history;
+	private Stack<SaveState> redoStack;
+	private SokobanObject[][] map;
+	private final int MAXUNDOS = 20;
+    private int prevRedoStackSize;
+
+	/**
+	 * Initialises a MapContainer of the given size filled with spaces
+	 */
+	public MapContainer(int xSize, int ySize) {
+		map = new SokobanObject[ySize][xSize];
+		for (SokobanObject[] row : map) {
+			Arrays.fill(row, SokobanObject.SPACE);
+		}
+		history = new FixedSizeStack<SaveState>(MAXUNDOS);
+		history.push(new SaveState(new Coordinate(-1, -1), new HashSet<Coordinate>()));
+		redoStack = new Stack<SaveState>();
+        prevRedoStackSize = 0;
+	}
+
+	public int getYSize() {
+		return map.length;
+	}
+
+	public int getXSize() {
+		return map[0].length;
+	}
+
+	public void storeState() {
+		history.push(new SaveState(getWPos(), getBoxPositions()));
+	}
+
+	/**
+	 * Pops a SaveState out of the history stack. If the stack is then empty,
+	 * the SaveState is put back in place as we require that there is always one
+	 * state to represent the positions of dynamic objects.
+	 *
+	 * @param sendToRedoStack   whether or not to place the popped SaveState
+	 *                          into the redo stack e.g. when the user inputs a
+	 *                          command
+	 */
+	public void undo(boolean sendToRedoStack) {
+		SaveState state = history.pop();
+		if (historyLength() == 0) {
+			history.push(state);
+		} else if (sendToRedoStack) {
+            prevRedoStackSize = redoStack.size();
+			redoStack.push(state);
+		}
+	}
+
+	/**
+	 * Puts an undone state back
+	 */
+	public void redo() {
+		if (redoStack.size() != 0) {
+            prevRedoStackSize = redoStack.size();
+			history.push(redoStack.pop());
+		}
+	}
+
+	public void clearRedoStack() {
+		redoStack.clear();
+	}
+
+	public int historyLength() {
+		return history.size();
+	}
+
+	public int totalHistoryLength() {
+		return history.getTotalSize();
+	}
+
+	/**
+	 * Checks the positions of all boxes to see if they've been placed on a
+	 * goal
+	 * TODO: Fix this.  The player should win if all goals have boxes on them. (Not vice versa like now.)
+	 * @return  true if all boxes are on a goal, false otherwise
+	 */
+	public boolean isDone() {
+		for (Coordinate coord : getBoxPositions()) {
+			if (get(coord) != SokobanObject.BOX_ON_GOAL) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Get the position of the worker
+	 *
+	 * @return  coordinates of the worker
+	 */
+	public Coordinate getWPos() {
+		return history.peek().getWPos();
+	}
+
+	public Set<Coordinate> getBoxPositions() {
+		return history.peek().getBoxPositions();
+	}
+
+	/**
+	 * Put a SokobanObject in the given position. See SokobanObject
+	 * documentation for an explanation of how objects are stored. Respects the
+	 * rules of the game e.g. can't place a PLAYER on top of a WALL.
+	 *
+	 * @param object    the object to be stored
+	 * @param coord     the coordinate at which to place it
+	 * @return          true if successful, false otherwise
+	 */
+	public boolean put(SokobanObject object, Coordinate coord) {
+		Set<Coordinate> boxPositions = getBoxPositions();
+		Coordinate wPos = getWPos();
+		SokobanObject target = get(coord);
+		int x = coord.getX();
+		int y = coord.getY();
+
+		if (object == SokobanObject.PLAYER || object == SokobanObject.PLAYER_ON_GOAL) {
+			switch(target) {
+			case SPACE:
+			case GOAL:      wPos = coord;
+			break;
+			default:        return false;
+			}
+			if (object == SokobanObject.PLAYER_ON_GOAL) {
+				map[y][x] = SokobanObject.GOAL;
+			}
+
+		} else if (object == SokobanObject.BOX || object == SokobanObject.BOX_ON_GOAL) {
+			switch(target) {
+			case SPACE:
+			case GOAL:  if (!boxHere(coord)) {
+				boxPositions.add(coord);
+			}
+			break;
+			default:    return false;
+			}
+			if (object == SokobanObject.BOX_ON_GOAL) {
+				map[y][x] = SokobanObject.GOAL;
+			}
+
+		} else if (object == SokobanObject.WALL) {
+			if (wPos.equals(coord) || boxHere(coord)) {
+				return false;
+			}
+			map[y][x] = SokobanObject.WALL;
+
+		} else {
+			map[y][x] = object;
+		}
+
+		history.pop();
+		history.push(new SaveState(wPos, boxPositions));
+		return true;
+	}
+
+	public boolean boxHere(Coordinate coord) {
+		return getBoxPositions().contains(coord);
+	}
+
+	/**
+	 * Removes a "layer" from the given coordinate. If there is a BOX or PLAYER
+	 * on a SPACE or GOAL, remove it; otherwise, make the coordinate a SPACE.
+	 *
+	 * @param coord     the coordinate to remove a layer from
+	 */
+	public void removeLayer(Coordinate coord) {
+		Set<Coordinate> boxPositions = getBoxPositions();
+		Coordinate wPos = getWPos();
+		if (wPos.equals(coord)) {
+			wPos = new Coordinate(-1, -1);
+		} else if (boxHere(coord)) {
+			boxPositions.remove(coord);
+		} else {
+			put(SokobanObject.SPACE, coord);
+		}
+		history.pop();
+		history.push(new SaveState(wPos, boxPositions));
+	}
+
+	/**
+	 * Get the type of SokobanObject at the given coordinate. Again, see the
+	 * SokobanObject documentation for an explanation of how objects are stored
+	 * and returned.
+	 */
+	public SokobanObject get(Coordinate coord) {
+		int x = coord.getX();
+		int y = coord.getY();
+		if (x > getXSize() - 1 || y > getYSize() - 1 || x < 0 || y < 0) {
+			return SokobanObject.WALL;
+		} else if (boxHere(coord)) {
+			if (map[y][x] == SokobanObject.GOAL) {
+				return SokobanObject.BOX_ON_GOAL;
+			}
+			return SokobanObject.BOX;
+		} else if (getWPos().equals(coord)) {
+			if (map[y][x] == SokobanObject.GOAL) {
+				return SokobanObject.PLAYER_ON_GOAL;
+			}
+			return SokobanObject.PLAYER;
+		}
+		return map[y][x];
+	}
+
+	public String toString() {
+		String mapLine = "";
+		for (int y = 0; y < getYSize(); y++) {
+			for (int x = 0; x < getXSize(); x++) {
+				Coordinate coord = new Coordinate(x, y);
+				mapLine = mapLine + get(coord).toString();
+			}
+			mapLine = mapLine + "\n";
+
+		}
+		return mapLine;
+	}
+
+	public Set<Coordinate> getChanges() {
+		Set<Coordinate> changedPlaces = new HashSet<Coordinate>();
+		SaveState[] stateArray = new SaveState[MAXUNDOS];
+		history.toArray(stateArray);
+		if (history.size() < 2 && redoStack.size() == 0) {
+			return changedPlaces;
+		}
+
+        SaveState lastState;
+        if (prevRedoStackSize < redoStack.size()) {
+            lastState = redoStack.peek();
+        } else {
+            lastState = stateArray[history.size() - 2];
+        }
+
+		if (!lastState.getWPos().equals(getWPos())) {
+			changedPlaces.add(getWPos());
+			changedPlaces.add(lastState.getWPos());
+		}
+
+		Set<Coordinate> currentBoxPositionsCopy = new HashSet<Coordinate>(getBoxPositions());
+		Set<Coordinate> lastBoxPositionsCopy = new HashSet<Coordinate>(lastState.getBoxPositions());
+		currentBoxPositionsCopy.removeAll(lastState.getBoxPositions());
+		lastBoxPositionsCopy.removeAll(getBoxPositions());
+		changedPlaces.addAll(currentBoxPositionsCopy);
+		changedPlaces.addAll(lastBoxPositionsCopy);
+		System.out.println(changedPlaces);
+		return changedPlaces;
+	}
+
+	/**
+	 * Takes a coordinate and returns the set of coordinates which are accessible from there.
+	 * Accessible meaning not blocked by a WALL.
+	 */
+	public Set<Coordinate> accessibleFrom(Coordinate origin) {
+		Set<Coordinate> edges = new HashSet<Coordinate>();
+		Set<Coordinate> accessible = new HashSet<Coordinate>();
+		Set<Coordinate> newEdges = new HashSet<Coordinate>();
+		edges.add(origin);
+		while (edges.size() != 0) {
+			for (Coordinate edge: edges) {
+				accessible.add(edge);
+				for (Coordinate potentialEdge : neighbors(edge)) {
+					if (get(potentialEdge) != SokobanObject.WALL) {
+                        newEdges.add(potentialEdge);
+                    }
+				}
+			}
+			edges.addAll(newEdges);
+			edges.removeAll(accessible);
+		}
+		return accessible;
+	}
+
+	/**
+	 * Takes a coordinate and returns its orthogonal neighbors (avoiding out of bounds areas).
+	 */
+	public Set<Coordinate> neighbors(Coordinate origin){
+		Set<Coordinate> neighbors = new HashSet<Coordinate>();
+		neighbors.add(origin.add(new Coordinate(1,0)));
+		neighbors.add(origin.add(new Coordinate(-1,0)));
+		neighbors.add(origin.add(new Coordinate(0,1)));
+		neighbors.add(origin.add(new Coordinate(0,-1)));
+		for(Coordinate potentialNeighbor : neighbors){
+			if (potentialNeighbor.getX()<0||potentialNeighbor.getX()>this.getXSize()||
+					potentialNeighbor.getY()<0||potentialNeighbor.getY()>this.getYSize()){
+				neighbors.remove(potentialNeighbor);
+			}
+		}
+		return neighbors;
+	}
+
+	public void growGrass(){
+		Set<Coordinate> potentialGrass = Coordinate.allValidCoordinates(getXSize(), getYSize());
+		potentialGrass.removeAll(accessibleFrom(getWPos()));
+		for(Coordinate potentialGrassSpace : potentialGrass){
+			if (get(potentialGrassSpace) == SokobanObject.SPACE) {
+                put(SokobanObject.GRASS, potentialGrassSpace);
+            }
+		}
+	}
+}
