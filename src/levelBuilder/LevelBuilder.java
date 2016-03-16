e6a5c255eb19109fdb05da7de4d3c41acfaade37
Marcus <marcusrc1@hotmail.co.uk>
2016-03-16 14:44:32 +0000

    latest release

--- src/levelBuilder/LevelBuilder.java
+++ src/levelBuilder/LevelBuilder.java
@@ -4,12 +4,31 @@ import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
 
 
-public class LevelBuilder{
-
+public class LevelBuilder
+{
+	// Grid variables:
+	private final static int x = 5;
+	private final static int y = 5;
+	private final static HashMap<String, JLabel> gridElements = new HashMap<String, JLabel>();
+	
+	// All variables & references for GUI:
+	private static JFrame frame;
+	private static JMenuBar menuBar;
+	private static JMenu file, edit, help;
+	private static JPanel mainPanel, editor, palette;
+	private static JMenuItem newMap, open, save, compile, exit, undo, redo;
+	private static ImageIcon wallIcon, goalIcon, playerIcon, boxIcon;
+	private static JLabel wall, goal, player, box;
+	
 	
 	public static void main(String[] args)
 	{
@@ -26,18 +45,6 @@ public class LevelBuilder{
 	
 	private static void createGUI()
 	{
-		/*
-		 *  Declare all variables used below:
-		 */
-		
-		JFrame frame;
-		JMenuBar menuBar;
-		JMenu file, edit, help;
-		JMenuItem newMap, open, save, compile, exit, undo, redo;
-		ImageIcon wallIcon, goalIcon, playerIcon, boxIcon;
-		JLabel wall, goal, player, box;
-		
-		 
 		// Setup Frame:
         frame = new JFrame("Level Builder");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -104,17 +111,19 @@ public class LevelBuilder{
         
         // Panel setup: The GUI uses three JPanels. A main JPanel, and two JPanels within:
         
-        JPanel mainPanel = new JPanel();
+        mainPanel = new JPanel();
         mainPanel.setBackground(Color.GREEN);
         mainPanel.setLayout(new BorderLayout());
         
-        JPanel editor = new JPanel();
+        editor = new JPanel();
         editor.setBackground(Color.BLACK);
-        JLabel editorTitle = new JLabel("Map Editor:");
-        editor.add(editorTitle);
+        editor.setSize(20, 3);
+        editor.setLayout(new GridLayout(y, x));
+        // Setup the 2d Array grid:
+        createGrid(editor, x, y);
 
-        
-        JPanel palette = new JPanel();
+        // Setup palette for selecting 'brush' for designing map:
+        palette = new JPanel();
         palette.setBackground(Color.RED);
         palette.setLayout(new BoxLayout(palette, BoxLayout.Y_AXIS));
         JLabel paletteTitle = new JLabel("Palette:");
@@ -139,7 +148,6 @@ public class LevelBuilder{
         palette.add(box);
 
         
-        
         // Setup Jpanel's grids layout:
         mainPanel.add(editor, BorderLayout.WEST);
         mainPanel.add(palette, BorderLayout.EAST);
@@ -154,4 +162,37 @@ public class LevelBuilder{
         
 	}
 	
+	private static void createGrid(JPanel panel, int x, int y)
+	{		
+        wallIcon = new ImageIcon(LevelBuilder.class.getResource("/tileset01/WALL.png"), "Wall");
+
+		// Outer loop for moving along the y axis:
+		for(int i=0; i<y; i++)
+		{
+			// Inner loop which is ran before the outer loop, moving along the x axis:
+			for(int j=0; j<x; j++)
+			{
+				// Save coordinates into a String:
+				String coords = ""+j+i;
+				// Add JLabel along with its coordinates into a HashMap:
+				LevelBuilder.gridElements.put(coords, new JLabel(wallIcon));
+				// Get it from HashMap and add it to the panel:
+				JLabel tempLabel = LevelBuilder.gridElements.get(coords);
+				tempLabel.addMouseListener(new MouseAdapter() 
+				{   
+
+			        public void mouseClicked(MouseEvent e)   
+			        {   
+			        	tempLabel.setText("Hello");
+			        }   
+				});	
+				panel.add(tempLabel);
+
+			}
+		}
+		
+		
+	}
 }
+
+	
