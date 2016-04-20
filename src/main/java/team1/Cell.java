17580cd7a389c19c0dc8543742607ef6a337383b
tpict <tompicton@tompicton.co.uk>
2016-04-20 20:45:42 +0100

    Fixed level builder regression - now uses GOAL.png instead of PRESSURE_PAD.png

--- src/main/java/team1/Cell.java
+++ src/main/java/team1/Cell.java
@@ -10,126 +10,126 @@ import javax.swing.JLabel;
 
 public class Cell extends JLabel{
 
-	private static final long serialVersionUID = 1L;
-	private boolean inUse;
-	private char tileType;
-	// Coordinate values: Named i and j so they don't affect LayoutManager.
-	private int i; // X Coordinate
-	private int j; // Y Coordinate
-	
-	public Cell(int i, int j)
-	{
-		super();
-		this.i = i;
-		this.j = j;
-		inUse = false;
-		tileType = 'z';
-		setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT.png"), "Default"));
-		
-		// Mouse listener:
-		addMouseListener(new MouseAdapter(){
-			
+    private static final long serialVersionUID = 1L;
+    private boolean inUse;
+    private char tileType;
+    // Coordinate values: Named i and j so they don't affect LayoutManager.
+    private int i; // X Coordinate
+    private int j; // Y Coordinate
 
-			public void mousePressed(MouseEvent me)
-			{
-				// If left mouse button is clicked
-				if (me.getButton() == MouseEvent.BUTTON1) {
-				// Change icon and TileType depending on current palette state:
-				
-				int choice = LevelBuilder.state;
-				
-				switch(choice) {
-				case 'w': setIcon(new ImageIcon(getClass().getResource("/tileset01/WALL.png"), "Wall"));
-				tileType = 'w';
-				break;
-				case 'b': setIcon(new ImageIcon(getClass().getResource("/tileset01/BOX.png"), "Box"));
-				TilePalette.boxCount = TilePalette.boxCount + 1;
-				TilePalette.boxCounter.setText("" + TilePalette.boxCount);	// increment box count
-				tileType = 'b';
-				break;
-				case 's': setIcon(new ImageIcon(getClass().getResource("/tileset01/SPACE.png"), "Space"));
-				tileType = 's';
-				break;
-				case 'p': setIcon(new ImageIcon(getClass().getResource("/tileset01/PRESSURE_PAD.png"), "Pressure Pad"));
-				TilePalette.pressureCount = TilePalette.pressureCount + 1;
-				TilePalette.pressureCounter.setText("" + TilePalette.pressureCount); // increment pressure pad count
-				tileType = 'p';
-				break;
-				case 'q': setIcon(new ImageIcon(getClass().getResource("/tileset01/PLAYER.png"), "Player"));
-				TilePalette.playerCount = TilePalette.playerCount + 1; //	increment player count
-				tileType = 'q';
-				break;
-				case 'h': setIcon(new ImageIcon(getClass().getResource("/tileset01/GRASS.png"), "Grass"));
-				tileType = 'h';
-				break;
-				
-				
-				}
-				inUse = true; // tile is in use if clicked
-				}
-				
-				
-				// If right mouse is clicked, undo 
-				// If right mouse is clicked, undo 
-				if (me.getButton() == MouseEvent.BUTTON3) {
-					
-					if (LevelBuilder.state == 'b' && TilePalette.boxCount != 0) {
-						TilePalette.boxCount -= 1;
-						TilePalette.boxCounter.setText("" + TilePalette.boxCount); // decrease box count
-					}
-					if (LevelBuilder.state == 'p' && TilePalette.pressureCount != 0) {
-						TilePalette.pressureCount -= 1;
-						TilePalette.pressureCounter.setText("" + TilePalette.pressureCount); // decrease pressure pad count
-					}
-					
-					setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT_HOVER.png"), "Default"));
-					tileType = 'z';
-					inUse = false;
-				}
-				
-			}
-			
-			//	Mouse hover
-			
-			public void mouseEntered(MouseEvent me) {
-				if (!inUse) {
-				setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT_HOVER.png")));
-				}
-			}
-			
-			public void mouseExited(MouseEvent me) {
-				if (!inUse) {
-				setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT.png")));
-				}
-			}	
+    public Cell(int i, int j)
+    {
+        super();
+        this.i = i;
+        this.j = j;
+        inUse = false;
+        tileType = 'z';
+        setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT.png"), "Default"));
 
-			
-		});
-	
-	}
-	
+        // Mouse listener:
+        addMouseListener(new MouseAdapter(){
 
-	// Accessor methods:
-	public int getI()
-	{
-		return i;
-	}
 
-	public int getJ()
-	{
-		return j;
-	}
-	
-	public boolean getInUse()
-	{
-		return inUse;
-	}
-	
-	public char getTileType()
-	{
-		return tileType;
-	}
-	
-	
-	
+            public void mousePressed(MouseEvent me)
+            {
+                // If left mouse button is clicked
+                if (me.getButton() == MouseEvent.BUTTON1) {
+                    // Change icon and TileType depending on current palette state:
+
+                    int choice = LevelBuilder.state;
+
+                    switch(choice) {
+                        case 'w': setIcon(new ImageIcon(getClass().getResource("/tileset01/WALL.png"), "Wall"));
+                                  tileType = 'w';
+                                  break;
+                        case 'b': setIcon(new ImageIcon(getClass().getResource("/tileset01/BOX.png"), "Box"));
+                                  TilePalette.boxCount = TilePalette.boxCount + 1;
+                                  TilePalette.boxCounter.setText("" + TilePalette.boxCount);	// increment box count
+                                  tileType = 'b';
+                                  break;
+                        case 's': setIcon(new ImageIcon(getClass().getResource("/tileset01/SPACE.png"), "Space"));
+                                  tileType = 's';
+                                  break;
+                        case 'p': setIcon(new ImageIcon(getClass().getResource("/tileset01/GOAL.png"), "Pressure Pad"));
+                                  TilePalette.pressureCount = TilePalette.pressureCount + 1;
+                                  TilePalette.pressureCounter.setText("" + TilePalette.pressureCount); // increment pressure pad count
+                                  tileType = 'p';
+                                  break;
+                        case 'q': setIcon(new ImageIcon(getClass().getResource("/tileset01/PLAYER.png"), "Player"));
+                                  TilePalette.playerCount = TilePalette.playerCount + 1; //	increment player count
+                                  tileType = 'q';
+                                  break;
+                        case 'h': setIcon(new ImageIcon(getClass().getResource("/tileset01/GRASS.png"), "Grass"));
+                                  tileType = 'h';
+                                  break;
+
+
+                    }
+                    inUse = true; // tile is in use if clicked
+                }
+
+
+                // If right mouse is clicked, undo
+                // If right mouse is clicked, undo
+                if (me.getButton() == MouseEvent.BUTTON3) {
+
+                    if (LevelBuilder.state == 'b' && TilePalette.boxCount != 0) {
+                        TilePalette.boxCount -= 1;
+                        TilePalette.boxCounter.setText("" + TilePalette.boxCount); // decrease box count
+                    }
+                    if (LevelBuilder.state == 'p' && TilePalette.pressureCount != 0) {
+                        TilePalette.pressureCount -= 1;
+                        TilePalette.pressureCounter.setText("" + TilePalette.pressureCount); // decrease pressure pad count
+                    }
+
+                    setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT_HOVER.png"), "Default"));
+                    tileType = 'z';
+                    inUse = false;
+                }
+
+            }
+
+            //	Mouse hover
+
+            public void mouseEntered(MouseEvent me) {
+                if (!inUse) {
+                    setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT_HOVER.png")));
+                }
+            }
+
+            public void mouseExited(MouseEvent me) {
+                if (!inUse) {
+                    setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT.png")));
+                }
+            }
+
+
+        });
+
+    }
+
+
+    // Accessor methods:
+    public int getI()
+    {
+        return i;
+    }
+
+    public int getJ()
+    {
+        return j;
+    }
+
+    public boolean getInUse()
+    {
+        return inUse;
+    }
+
+    public char getTileType()
+    {
+        return tileType;
+    }
+
+
+
 }
--- src/main/java/team1/TilePalette.java
+++ src/main/java/team1/TilePalette.java
@@ -8,94 +8,94 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.Dimension;
 
 public class TilePalette extends JPanel {
-	
-	private static final long serialVersionUID = 1L;
-	private static final ImageIcon[] tiles = new ImageIcon[6];
-	private static final JList<ImageIcon> list = new JList<ImageIcon>(tiles);
-	protected static TextField boxCounter = new TextField(10); 
-	protected static TextField pressureCounter = new TextField(10);
-	protected static int boxCount;
-	protected static int pressureCount;
-	protected static int playerCount;
-	
-	public TilePalette()
-	{
-		super();
-		importImages();
-		add(new JLabel("Palette"));
-		setBackground(Color.WHITE);
-		setPreferredSize(new Dimension(100, 80));
-		formatList();
-		setCounters();
-		list.addListSelectionListener(new ListListener());
-	}
-	
-	public void formatList() {
-		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-		list.setLayoutOrientation(JList.VERTICAL_WRAP);
-		list.setFixedCellHeight(36);
-		list.setFixedCellWidth(34); // rather than 32 to allow border
-		list.setVisibleRowCount(6);
-		list.setBackground(Color.GRAY);
-		list.setForeground(Color.BLACK);
-		add(list);
-	}
-	
-	public void importImages() {
-		
-		ImageIcon wall = new ImageIcon(getClass().getResource("/tileset01/WALL.png"), "Wall");
-		ImageIcon box = new ImageIcon(getClass().getResource("/tileset01/BOX.png"), "Box");
-		ImageIcon space = new ImageIcon(getClass().getResource("/tileset01/SPACE.png"), "Space");
-		ImageIcon pressure = new ImageIcon(getClass().getResource("/tileset01/PRESSURE_PAD.png"), "Pressure Pad");
-		ImageIcon player = new ImageIcon(getClass().getResource("/tileset01/PLAYER.png"), "Player");
-		ImageIcon grass = new ImageIcon(getClass().getResource("/tileset01/GRASS.png"), "Grass");
-		
-		tiles[0] = wall;
-		tiles[1] = box;
-		tiles[2] = space;
-		tiles[3] = pressure;
-		tiles[4] = player;
-		tiles[5] = grass;
-	}
-	
-	public void setCounters() {
-		add(new Label("Boxes:"));
-		boxCounter.setText("" + boxCount);
-		add(boxCounter);   
-		add(new Label("Pressure Pads:"));
-		pressureCounter.setText("" + pressureCount);
-		add(pressureCounter);
-		boxCounter.setEditable(false);   
-		pressureCounter.setEditable(false);
-	}
-	
-	public void selectTile() {
 
-		int selection = list.getSelectedIndex();
-		switch(selection) {
-		case 0: LevelBuilder.state = 'w'; // wall
-		break;
-		case 1: LevelBuilder.state = 'b'; // box
-		break;
-		case 2: LevelBuilder.state = 's'; // space (floor)
-		break;
-		case 3: LevelBuilder.state = 'p'; // pressure pad
-		break;
-		case 4: LevelBuilder.state = 'q'; // player
-		break;
-		case 5: LevelBuilder.state = 'h'; // grass
-		break;
-		}
-	}
-	
-	class ListListener implements ListSelectionListener {
-	
-	@Override
-	public void valueChanged(ListSelectionEvent e) {
-		 if (!e.getValueIsAdjusting()) {
-			 selectTile();
-	}
-	}
-	
-}
+    private static final long serialVersionUID = 1L;
+    private static final ImageIcon[] tiles = new ImageIcon[6];
+    private static final JList<ImageIcon> list = new JList<ImageIcon>(tiles);
+    protected static TextField boxCounter = new TextField(10);
+    protected static TextField pressureCounter = new TextField(10);
+    protected static int boxCount;
+    protected static int pressureCount;
+    protected static int playerCount;
+
+    public TilePalette()
+    {
+        super();
+        importImages();
+        add(new JLabel("Palette"));
+        setBackground(Color.WHITE);
+        setPreferredSize(new Dimension(100, 80));
+        formatList();
+        setCounters();
+        list.addListSelectionListener(new ListListener());
+    }
+
+    public void formatList() {
+        list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        list.setLayoutOrientation(JList.VERTICAL_WRAP);
+        list.setFixedCellHeight(36);
+        list.setFixedCellWidth(34); // rather than 32 to allow border
+        list.setVisibleRowCount(6);
+        list.setBackground(Color.GRAY);
+        list.setForeground(Color.BLACK);
+        add(list);
+    }
+
+    public void importImages() {
+
+        ImageIcon wall = new ImageIcon(getClass().getResource("/tileset01/WALL.png"), "Wall");
+        ImageIcon box = new ImageIcon(getClass().getResource("/tileset01/BOX.png"), "Box");
+        ImageIcon space = new ImageIcon(getClass().getResource("/tileset01/SPACE.png"), "Space");
+        ImageIcon pressure = new ImageIcon(getClass().getResource("/tileset01/GOAL.png"), "Pressure Pad");
+        ImageIcon player = new ImageIcon(getClass().getResource("/tileset01/PLAYER.png"), "Player");
+        ImageIcon grass = new ImageIcon(getClass().getResource("/tileset01/GRASS.png"), "Grass");
+
+        tiles[0] = wall;
+        tiles[1] = box;
+        tiles[2] = space;
+        tiles[3] = pressure;
+        tiles[4] = player;
+        tiles[5] = grass;
+    }
+
+    public void setCounters() {
+        add(new Label("Boxes:"));
+        boxCounter.setText("" + boxCount);
+        add(boxCounter);
+        add(new Label("Pressure Pads:"));
+        pressureCounter.setText("" + pressureCount);
+        add(pressureCounter);
+        boxCounter.setEditable(false);
+        pressureCounter.setEditable(false);
+    }
+
+    public void selectTile() {
+
+        int selection = list.getSelectedIndex();
+        switch(selection) {
+            case 0: LevelBuilder.state = 'w'; // wall
+                    break;
+            case 1: LevelBuilder.state = 'b'; // box
+                    break;
+            case 2: LevelBuilder.state = 's'; // space (floor)
+                    break;
+            case 3: LevelBuilder.state = 'p'; // pressure pad
+                    break;
+            case 4: LevelBuilder.state = 'q'; // player
+                    break;
+            case 5: LevelBuilder.state = 'h'; // grass
+                    break;
+        }
+    }
+
+    class ListListener implements ListSelectionListener {
+
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            if (!e.getValueIsAdjusting()) {
+                selectTile();
+            }
+        }
+
+    }
 }
