692a51de295c53e3ea36a3f0e392f42efde5437e
Joshua Gant <joshdgant@gmail.com>
2016-04-21 19:55:47 +0100

    Indentation.  WHATEVER.

--- src/main/java/team1/BoxTerm.java
+++ src/main/java/team1/BoxTerm.java
@@ -93,137 +93,137 @@ public class BoxTerm extends JPanel {
 		default:                return;
 		}
 		redraw();
-		}
-
-		private static void makeMenuBar(JFrame frame)
-		{
-			final int SHORTCUT_MASK =
-					Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
-
-			JMenuBar menubar = new JMenuBar();
-			frame.setJMenuBar(menubar);
-
-			// create the File manu
-			JMenu fileMenu = new JMenu("File");
-			menubar.add(fileMenu);
-			
-			JMenu viewMenu = new JMenu("View");
-			menubar.add(viewMenu);
-
-			JMenu helpMenu = new JMenu("Help");
-			menubar.add(helpMenu);
-
-			JMenuItem openItem = new JMenuItem("Open");
-			openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, SHORTCUT_MASK));
-			openItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					// GOT TO SORT THIS OUT.  Possibly put this and repeated code in main method in importLevel().
-					try {
-						SokobanMap.importLevel(getFile());
-					} catch (FileNotFoundException e1) {
-						// TODO Sort out some verification here.
-						System.out.println("BAD LEVEL");
-						e1.printStackTrace();
-					}
-					frame.remove(spriteMap);
-					spriteMap = new SpriteMap(map, true, 1);
-					redraw();
-					frame.add(spriteMap);
-					frame.pack();
-				}
-			});
-			fileMenu.add(openItem);
-
-			JMenuItem levelBuilderItem = new JMenuItem("Start level builder");
-			levelBuilderItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					LevelBuilder.activate();
-				}
-			});
-			fileMenu.add(levelBuilderItem);
-
-			JMenuItem quitItem = new JMenuItem("Quit");
-			quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
-			quitItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) { System.exit(0);; }
-			});
-			fileMenu.add(quitItem);
-			
-			JMenuItem tileItem = new JMenuItem("Change Tileset");
-			tileItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, SHORTCUT_MASK));
-			tileItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					tileSetNo++;
-					spriteMap.loadSprites(tileSetNo%3);; }
-			});
-			viewMenu.add(tileItem);
-			
-			JMenuItem magnifyItem = new JMenuItem("Increase Magnification");
-			magnifyItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					spriteMap.changeMagnification(true);
-					spriteMap.loadSprites(tileSetNo%3);
-					frame.pack(); }
-			});
-			viewMenu.add(magnifyItem);
-			
-			JMenuItem deMagnifyItem = new JMenuItem("Decrease Magnification");
-			deMagnifyItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					spriteMap.changeMagnification(false);
-					spriteMap.placeSprites();
-					frame.pack(); }
-			});
-			viewMenu.add(deMagnifyItem);
-
-			JMenuItem aboutItem = new JMenuItem("About Box Terminator");
-			aboutItem.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					JOptionPane.showMessageDialog(spriteMap, "A Sokoban clone.", "Box Terminator",JOptionPane.PLAIN_MESSAGE,spriteMap.getPlayerSprite());
-				}
-			});
-			helpMenu.add(aboutItem);
-		}
-
-		//TODO The getFile method should check for a valid Sokoban level file and force the user to choose another level
-		public static InputStream getFile() throws FileNotFoundException
-		{
-			int returnVal = fileChooser.showOpenDialog(null);
-			if(returnVal != JFileChooser.APPROVE_OPTION) {
-				return null;  // cancelled
-			}
-			File selectedFile = fileChooser.getSelectedFile();
-			InputStream streamToReturn = new FileInputStream(selectedFile);
-			return streamToReturn;
-		}
-
-		/**
-		 * Updates the contents of the game window
-		 */
-		public static void redraw() {
-			textArea.setText(map.toString());
-			textArea.append(Integer.toString(map.totalHistoryLength() - 1));
-			// New line here to redraw the spriteMap.
-			spriteMap.placeSprites();
-		}
-
-		public static void main(String[] args) {
-			// importLevel(new File("src/main/resources/level"));
-			InputStream level = BoxTerm.class.getClassLoader().getResourceAsStream("level");
-			map = SokobanMap.importLevel(level);
-			map.growGrass();
-			BoxTerm boxterm = new BoxTerm();
-			JFrame frame = new JFrame("Box Terminator");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			textArea = new JTextArea(ySize + 1, xSize + 1);
-			textArea.setFont(new Font("monospaced", Font.PLAIN, 24));
-			textArea.setEditable(false);
-			spriteMap = new SpriteMap(map, true, 1);
-			redraw();
-			frame.add(boxterm);
-			frame.add(spriteMap);
-			makeMenuBar(frame);
-			frame.pack();
-			frame.setVisible(true);
-		}
 	}
+
+	private static void makeMenuBar(JFrame frame)
+	{
+		final int SHORTCUT_MASK =
+				Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+
+		JMenuBar menubar = new JMenuBar();
+		frame.setJMenuBar(menubar);
+
+		// create the File manu
+		JMenu fileMenu = new JMenu("File");
+		menubar.add(fileMenu);
+
+		JMenu viewMenu = new JMenu("View");
+		menubar.add(viewMenu);
+
+		JMenu helpMenu = new JMenu("Help");
+		menubar.add(helpMenu);
+
+		JMenuItem openItem = new JMenuItem("Open");
+		openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, SHORTCUT_MASK));
+		openItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// GOT TO SORT THIS OUT.  Possibly put this and repeated code in main method in importLevel().
+				try {
+					SokobanMap.importLevel(getFile());
+				} catch (FileNotFoundException e1) {
+					// TODO Sort out some verification here.
+					System.out.println("BAD LEVEL");
+					e1.printStackTrace();
+				}
+				frame.remove(spriteMap);
+				spriteMap = new SpriteMap(map, true, 1);
+				redraw();
+				frame.add(spriteMap);
+				frame.pack();
+			}
+		});
+		fileMenu.add(openItem);
+
+		JMenuItem levelBuilderItem = new JMenuItem("Start level builder");
+		levelBuilderItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LevelBuilder.activate();
+			}
+		});
+		fileMenu.add(levelBuilderItem);
+
+		JMenuItem quitItem = new JMenuItem("Quit");
+		quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
+		quitItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { System.exit(0);; }
+		});
+		fileMenu.add(quitItem);
+
+		JMenuItem tileItem = new JMenuItem("Change Tileset");
+		tileItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, SHORTCUT_MASK));
+		tileItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tileSetNo++;
+				spriteMap.loadSprites(tileSetNo%3);; }
+		});
+		viewMenu.add(tileItem);
+
+		JMenuItem magnifyItem = new JMenuItem("Increase Magnification");
+		magnifyItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				spriteMap.changeMagnification(true);
+				spriteMap.loadSprites(tileSetNo%3);
+				frame.pack(); }
+		});
+		viewMenu.add(magnifyItem);
+
+		JMenuItem deMagnifyItem = new JMenuItem("Decrease Magnification");
+		deMagnifyItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				spriteMap.changeMagnification(false);
+				spriteMap.placeSprites();
+				frame.pack(); }
+		});
+		viewMenu.add(deMagnifyItem);
+
+		JMenuItem aboutItem = new JMenuItem("About Box Terminator");
+		aboutItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(spriteMap, "A Sokoban clone.", "Box Terminator",JOptionPane.PLAIN_MESSAGE,spriteMap.getPlayerSprite());
+			}
+		});
+		helpMenu.add(aboutItem);
+	}
+
+	//TODO The getFile method should check for a valid Sokoban level file and force the user to choose another level
+	public static InputStream getFile() throws FileNotFoundException
+	{
+		int returnVal = fileChooser.showOpenDialog(null);
+		if(returnVal != JFileChooser.APPROVE_OPTION) {
+			return null;  // cancelled
+		}
+		File selectedFile = fileChooser.getSelectedFile();
+		InputStream streamToReturn = new FileInputStream(selectedFile);
+		return streamToReturn;
+	}
+
+	/**
+	 * Updates the contents of the game window
+	 */
+	public static void redraw() {
+		textArea.setText(map.toString());
+		textArea.append(Integer.toString(map.totalHistoryLength() - 1));
+		// New line here to redraw the spriteMap.
+		spriteMap.placeSprites();
+	}
+
+	public static void main(String[] args) {
+		// importLevel(new File("src/main/resources/level"));
+		InputStream level = BoxTerm.class.getClassLoader().getResourceAsStream("level");
+		map = SokobanMap.importLevel(level);
+		map.growGrass();
+		BoxTerm boxterm = new BoxTerm();
+		JFrame frame = new JFrame("Box Terminator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		textArea = new JTextArea(ySize + 1, xSize + 1);
+		textArea.setFont(new Font("monospaced", Font.PLAIN, 24));
+		textArea.setEditable(false);
+		spriteMap = new SpriteMap(map, true, 1);
+		redraw();
+		frame.add(boxterm);
+		frame.add(spriteMap);
+		makeMenuBar(frame);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
