3cb277c28507234eb191803e2fc649415144ceed
tpict <tompicton@tompicton.co.uk>
2016-04-22 13:23:26 +0100

    Fixed bug in FixedSizeStack.reset(T object)
    Previously didn't remove every object

--- src/main/java/team1/FixedSizeStack.java
+++ src/main/java/team1/FixedSizeStack.java
@@ -33,7 +33,7 @@ public class FixedSizeStack<T> extends Stack<T> {
     }
 
     public void reset(T object) {
-        while (this.size() > 1) {
+        while (this.size() > 0) {
             super.pop();
         }
         super.push(object);
