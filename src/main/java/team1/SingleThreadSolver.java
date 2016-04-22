5bd996398935551018c533424a238dcbaf143e6c
tpict <tompicton@tompicton.co.uk>
2016-04-22 15:59:00 +0100

    Trying to make sense of SingleThreadSolver

--- src/main/java/team1/SingleThreadSolver.java
+++ src/main/java/team1/SingleThreadSolver.java
@@ -5,41 +5,40 @@ import java.util.List;
 import java.util.Set;
 
 public class SingleThreadSolver {
-	private SokobanMap map;
-	private List<SaveState> seenStates;
-	private List<Integer> stateOrigins;
-	private List<Coordinate[]> pushes;
+    private SokobanMap map;
+    private List<SaveState> seenStates;
+    private List<Integer> stateOrigins;
+    private List<Coordinate[]> pushes;
 
-	public SingleThreadSolver(SokobanMap map){
-		this.map=map;
-		seenStates = new ArrayList<SaveState>();
-		stateOrigins = new ArrayList<Integer>();
-		pushes = new ArrayList<Coordinate[]>();		
-	}
+    public SingleThreadSolver(SokobanMap map) {
+        this.map = map;
+        seenStates = new ArrayList<SaveState>();
+        stateOrigins = new ArrayList<Integer>();
+        pushes = new ArrayList<Coordinate[]>();
+    }
 
-	private List<Coordinate[]> validPushes(SaveState state){
-		Set<Coordinate> accessibleSpaces = map.accessibleSpaces(state.getWPos(), false);
-		Set<Coordinate> allAccessibleSpaces = map.accessibleSpaces(state.getWPos(), true);
-		List<Coordinate[]> validPushes = new ArrayList<Coordinate[]>();
-		for (Coordinate box : state.getBoxPositions()){
-			for (Coordinate spaceNextToBox : map.neighbors(box))
-				if (accessibleSpaces.contains(spaceNextToBox)&&
-						map.get(box.add(box.add(spaceNextToBox.reverse()))).name().equals("SPACE") ||
-						map.get(box.add(box.add(spaceNextToBox.reverse()))).name().equals("GOAL")){
-					Coordinate[] aPush = {spaceNextToBox,box.add(spaceNextToBox.reverse())};
-					validPushes.add(aPush);
-				}
-		}
-		return validPushes;
-	}
-	
-	public String validPushesTestString(){
-		List<Coordinate[]> validPushes = validPushes(map.getInitialState());
-		String allPushesString = "";
-		for (int i=0;i<validPushes.size();i++){
-		 allPushesString+=(validPushes.get(i)[0]+"	 Direction: "+validPushes.get(i)[1]+"\n");
-		}
-		return allPushesString;
-	}
+    private List<Coordinate[]> validPushes(SaveState state) {
+        Set<Coordinate> accessibleSpaces = map.accessibleSpaces(state.getWPos(), false);
+        Set<Coordinate> allAccessibleSpaces = map.accessibleSpaces(state.getWPos(), true);
+        List<Coordinate[]> validPushes = new ArrayList<Coordinate[]>();
+        for (Coordinate box : state.getBoxPositions()) {
+            for (Coordinate spaceNextToBox : map.neighbors(box))
+                if (accessibleSpaces.contains(spaceNextToBox) &&
+                        map.get(box.add(box.add(spaceNextToBox.reverse()))) == SokobanObject.SPACE ||
+                        map.get(box.add(box.add(spaceNextToBox.reverse()))) == SokobanObject.GOAL) {
+                    Coordinate[] aPush = {spaceNextToBox, box.add(spaceNextToBox.reverse())};
+                    validPushes.add(aPush);
+            }
+        }
+        return validPushes;
+    }
 
+    public String validPushesTestString() {
+        List<Coordinate[]> validPushes = validPushes(map.getInitialState());
+        String allPushesString = "";
+        for (int i=0; i < validPushes.size(); i++) {
+            allPushesString += validPushes.get(i)[0] + " 	  Direction: " + validPushes.get(i)[1] + "\n";
+        }
+        return allPushesString;
+    }
 }
