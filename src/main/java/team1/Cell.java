5a180913d1020b6a2bb192775e77b4e71dc15d00
tpict <tompicton@tompicton.co.uk>
2016-04-22 10:38:44 +0100

    Removed some cruft from Cell

--- src/main/java/team1/Cell.java
+++ src/main/java/team1/Cell.java
@@ -3,17 +3,12 @@ package team1;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-
-// import levelBuilder.TilePalette.ListListener;
 
 public class Cell extends JLabel{
 
     private static final long serialVersionUID = 1L;
-    private SokobanObject tileType;
     private SpriteMap spriteMap;
-    // Coordinate values: Named i and j so they don't affect LayoutManager.
     private Coordinate position;
     private boolean playable;
 
@@ -22,8 +17,6 @@ public class Cell extends JLabel{
         this.playable=playable;
         this.position = position;
         this.spriteMap = spriteMap;
-        tileType = SokobanObject.SPACE;
-//        setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT.png"), "Default"));
 
         // Mouse listener:
         if (!this.playable) {
@@ -35,13 +28,10 @@ public class Cell extends JLabel{
                     // If left mouse button is clicked
                     if (me.getButton() == MouseEvent.BUTTON1) {
                         spriteMap.getMap().put(LevelBuilder.state, position);
-                        tileType = LevelBuilder.state;
                     }
 
                     if (me.getButton() == MouseEvent.BUTTON3) {
-//                        setIcon(new ImageIcon(getClass().getResource("/tileset01/DEFAULT_HOVER.png"), "Default"));
                         spriteMap.getMap().removeLayer(position);
-                        tileType = SokobanObject.SPACE;
                     }
                     spriteMap.placeSprites();
                     spriteMap.getMap().storeState();
@@ -55,26 +45,20 @@ public class Cell extends JLabel{
                 //	Mouse hover
 
                 public void mouseEntered(MouseEvent me) {
-                    if (tileType.equals(SokobanObject.SPACE)) {
+                    if (spriteMap.getMap().get(position) == SokobanObject.SPACE) {
                         setIcon(spriteMap.getIconMap().get("DEFAULT_HOVER"));
                     }
                 }
 
                 public void mouseExited(MouseEvent me) {
-                	if(spriteMap.getMap().get(position).name().equals("SPACE")){
-                		setIcon(spriteMap.getIconMap().get("DEFAULT"));
-                	}
-                	else{
-                    setIcon(spriteMap.getIconMap().get(spriteMap.getMap().get(position).name()));
-                	}
+                    if(spriteMap.getMap().get(position) == SokobanObject.SPACE){
+                        setIcon(spriteMap.getIconMap().get("DEFAULT"));
+                    } else {
+                        setIcon(spriteMap.getIconMap().get(spriteMap.getMap().get(position).name()));
+                    }
                 }
             });
         }
-    }
-
-    public SokobanObject getTileType()
-    {
-        return tileType;
     }
 
     public Coordinate getPosition() {
