RosieBellini/lbuilder

commit b4bcd3afbebf0b49582adacc4368a164ad2ab341

Handle cancel solver/no solution found separately
@Gantonator this seems to add a slight delay to the cancel button. Are
we ok with this?

src/main/java/team1/BoxTerm.java
@@ -81,13 +81,18 @@ public class BoxTerm extends JPanel {
 
             @Override
             protected void done() {
-                if (solution.size() != 0) {
-                    SokobanGame.getSpriteMap().reset();
-                    SokobanGame.getSpriteMap().setSolution(solution);
-                    SokobanGame.redraw();
+                solving = false;
+
+                if (solution != null) {
+                    if (solution.size() > 0) {
+                        SokobanGame.getSpriteMap().reset();
+                        SokobanGame.getSpriteMap().setSolution(solution);
+                        SokobanGame.redraw();
+                    } else {
+                        System.out.println("Impossible level!");
+                    }
                 }
 
-                solving = false;
                 dialog.dispose();
             }
 

src/main/java/team1/SingleThreadSolver.java
@@ -36,7 +36,7 @@ public class SingleThreadSolver implements Runnable {
     }
 
     public void stopSolving() {
-        solving = false;
+        stopped = true;
     }
 
     public boolean isSolving() {

commit 6a27ad847b06cfad0e693216b4607194ad54f9f9

Added "impossible level" dialogue (fix #12)
This also fixes the "delayed cancellation" bug introduced in b4bcd3afbebf0b49582adacc4368a164ad2ab341.
I haven't been able to get this to happen, but there's a chance that if
you cancel the dialogue JUST as the solver finishes something will go
wrong.

src/main/java/team1/BoxTerm.java
@@ -76,44 +76,17 @@ public class BoxTerm extends JPanel {
         final JDialog dialog = new JDialog();
         JPanel panel = new JPanel(new BorderLayout(5, 5));
 
-        class solverWorker extends SwingWorker<Void, Object> {
-            HashMap<SaveState, Coordinate[]> solution;
-
-            @Override
-            protected void done() {
-                solving = false;
-
-                if (solution != null) {
-                    if (solution.size() > 0) {
-                        SokobanGame.getSpriteMap().reset();
-                        SokobanGame.getSpriteMap().setSolution(solution);
-                        SokobanGame.redraw();
-                    } else {
-                        System.out.println("Impossible level!");
-                    }
-                }
-
-                dialog.dispose();
-            }
-
-            @Override
-            protected Void doInBackground() throws Exception {
-                solving = true;
-                SokobanMap mapToSolve = new SokobanMap(SokobanGame.getSokobanMap());
-                solver = new SingleThreadSolver(mapToSolve);
-                solution = solver.levelSolution();
-                return null;
-            }
-        };
-
         ImageIcon spinnyCube = new ImageIcon(Toolkit.getDefaultToolkit().getImage(BoxTerm.class.getResource("/tileset01/cube.gif")));
         JLabel loadingCube = new JLabel(spinnyCube);
+        ImageIcon impossibleCube = new ImageIcon(Toolkit.getDefaultToolkit().getImage(BoxTerm.class.getResource("/tileset01/IMPOSSIBLE.png")));
+        JLabel badCube = new JLabel(impossibleCube);
         JLabel msgLabel = new JLabel("Calculating...");
         JButton b1 = new JButton("Cancel");
 
         ActionListener listen = new ActionListener() {
             public void actionPerformed(ActionEvent e2) {
                 solver.stopSolving();
+                dialog.dispose();
             }
         };
 
@@ -132,6 +105,41 @@ public class BoxTerm extends JPanel {
         dialog.setLocationRelativeTo(frame);
         dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
         dialog.setVisible(true);
+
+        class solverWorker extends SwingWorker<Void, Object> {
+            HashMap<SaveState, Coordinate[]> solution;
+
+            @Override
+            protected void done() {
+                solving = false;
+
+                if (solution != null) {
+                    if (solution.size() > 0) {
+                        SokobanGame.getSpriteMap().reset();
+                        SokobanGame.getSpriteMap().setSolution(solution);
+                        SokobanGame.redraw();
+                        dialog.dispose();
+                    } else {
+                        msgLabel.setText("This level is impossible!");
+                        panel.remove(loadingCube);
+                        panel.add(badCube);
+                        panel.repaint();
+                        panel.setSize(panel.getPreferredSize());
+                    }
+                } else {
+                    dialog.dispose();
+                }
+            }
+
+            @Override
+            protected Void doInBackground() throws Exception {
+                solving = true;
+                SokobanMap mapToSolve = new SokobanMap(SokobanGame.getSokobanMap());
+                solver = new SingleThreadSolver(mapToSolve);
+                solution = solver.levelSolution();
+                return null;
+            }
+        };
 
         (new solverWorker()).execute();
     }