a93917df8a71ec806823bd37544c093e97dea16e
tpict <tompicton@tompicton.co.uk>
2016-05-02 15:15:56 +0100

    SpriteLabel docs

--- src/main/java/team1/SpriteLabel.java
+++ src/main/java/team1/SpriteLabel.java
@@ -5,7 +5,11 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
 
-public class SpriteLabel extends JLabel{
+/**
+ * Represents a single position in the MapPanel and provides a mouse listener
+ * to enable player interaction with it.
+ */
+public class SpriteLabel extends JLabel {
     private static final long serialVersionUID = 1L;
     private MapPanel mapPanel;
     private Coordinate position;
@@ -14,96 +18,146 @@ public class SpriteLabel extends JLabel{
     private static boolean mouseDown = false;
     private static boolean rightMouseDown = false;
 
+    /**
+     * SpriteLabel constructor.
+     *
+     * @param   position        The position of this SpriteLabel in the game map
+     * @param   mapPanel        The MapPanel that this SpriteLabel belongs to
+     */
     public SpriteLabel(Coordinate position, MapPanel mapPanel) {
         super();
         this.position = position;
         this.mapPanel = mapPanel;
 
-        addMouseListener(new MouseAdapter(){
-            public void mousePressed(MouseEvent me)
-            {       
+        addMouseListener(new MouseAdapter() {
+
+            // Sets mouse status fields to held and either moves the player or
+            // modifies the object at this position depending on mode
+            public void mousePressed(MouseEvent me) {
                 if (!mapPanel.getPlayable()) {
-                    if (me.getButton()==MouseEvent.BUTTON1){
-                        mouseDown=true;
+                    if (me.getButton() == MouseEvent.BUTTON1) {
+                        mouseDown = true;
                     }
-                    if (me.getButton()==MouseEvent.BUTTON3){
-                        rightMouseDown=true;
-                    }         
+                    if (me.getButton() == MouseEvent.BUTTON3) {
+                        rightMouseDown = true;
+                    }
+
                     modifyCell(me);
+
                 } else {
                     move();
                 }
             }
 
-            public void mouseReleased(MouseEvent me){
-                if (!mapPanel.getPlayable()){
-                    if (me.getButton()==MouseEvent.BUTTON1){
-                        mouseDown=false;
+            // Sets mouse status fields to released
+            public void mouseReleased(MouseEvent me) {
+                if (!mapPanel.getPlayable()) {
+                    if (me.getButton() == MouseEvent.BUTTON1) {
+                        mouseDown = false;
                     }
-                    if (me.getButton()==MouseEvent.BUTTON3){
-                        rightMouseDown=false;
-                    }               
+                    if (me.getButton() == MouseEvent.BUTTON3) {
+                        rightMouseDown = false;
+                    }
                 }
             }
 
+            //Shows DEFAULT_HOVER icon in editor mode
             public void mouseEntered(MouseEvent me) {
+                SokobanObject object = mapPanel.getSokobanMap().get(position);
+
                 if (!mapPanel.getPlayable()) {
-                    if (mouseDown || rightMouseDown){
+                    if (mouseDown || rightMouseDown) {
                         modifyCell(me);
                     }
+
                     if (me.getButton() == MouseEvent.NOBUTTON) {
-                        if (mapPanel.getSokobanMap().get(position) == SokobanObject.SPACE) {
+                        if (object == SokobanObject.SPACE) {
                             setIcon(mapPanel.getIconMap().get("DEFAULT_HOVER"));
                         }
+
                     } else {
                         modifyCell(me);
                     }
                 }
             }
 
+            //Removes DEFAULT_HOVER icon in editor mode
             public void mouseExited(MouseEvent me) {
+                SokobanObject object = mapPanel.getSokobanMap().get(position);
+
                 if (!mapPanel.getPlayable()) {
-                    if (mapPanel.getSokobanMap().get(position) == SokobanObject.SPACE) {
+                    if (object == SokobanObject.SPACE) {
                         setIcon(mapPanel.getIconMap().get("DEFAULT"));
                     } else {
-                        setIcon(mapPanel.getIconMap().get(mapPanel.getSokobanMap().get(position).name()));
+                        setIcon(mapPanel.getIconMap().get(object.name()));
                     }
                 }
             }
         });
     }
 
+    /**
+     * Changes the SokobanObject at the position of this SpriteLabel to the same
+     * type as that selected in the tile palette.
+     *
+     * @param   me      The mouse event to trigger this action on
+     */
     private void modifyCell(MouseEvent me) {
-        mapPanel.getSokobanMap().storeState();
-        if (mouseDown || me.getButton() == MouseEvent.BUTTON1) {
-            mapPanel.getSokobanMap().put(paletteState, position);
-        } else if (rightMouseDown || me.getButton() == MouseEvent.BUTTON3) {
-            if (mapPanel.getSokobanMap().get(position) != SokobanObject.PLAYER && mapPanel.getSokobanMap().get(position) != SokobanObject.PLAYER_ON_GOAL) {
-                mapPanel.getSokobanMap().removeLayer(position);
+        SokobanMap map = mapPanel.getSokobanMap();
+        map.storeState();
+
+        if (mouseDown) {
+            map.put(paletteState, position);
+        } else if (rightMouseDown) {
+            SokobanObject object = map.get(position);
+            if (object != SokobanObject.PLAYER
+                    || object != SokobanObject.PLAYER_ON_GOAL) {
+                map.removeLayer(position);
             }
         }
+
         GamePanel.redraw();
-        mapPanel.getSokobanMap().clearRedoStack();
+        map.clearRedoStack();
     }
 
+    /**
+     * Sets the type of SokobanObject to place in editor mode.
+     *
+     * @param   paletteState        The type of SokobanObject selected in the
+     *                              tile palette
+     */
     public static void setPaletteState(SokobanObject paletteState) {
         SpriteLabel.paletteState = paletteState;
     }
 
+    /**
+     * Returns the SokobanMap.Mover used by this class for click movement.
+     *
+     * @return      The value of the static field mover
+     */
     public static SokobanMap.Mover getMover() {
         return mover;
     }
 
-    private void move(){
+    /**
+     * Moves the player to the position of this SpriteLabel, interrupting
+     * moves in execution if necessary, and pushing boxes if allowed.
+     */
+    private void move() {
         SokobanMap map = mapPanel.getSokobanMap();
-        if (map.neighbours(position).contains(map.getState().getPlayerPos())){
-            map.move(position.add(map.getState().getPlayerPos().reverse()));
+        Coordinate playerPosition = map.getState().getPlayerPos();
+
+        if (map.neighbours(position).contains(playerPosition)) {
+            map.move(position.add(playerPosition.reverse()));
             GamePanel.redraw();
+
         } else {
             if (!(map.getIsCurrentlyMoving() || map.getIsDoingSolution())) {
                 mover = map.new Mover(position, 75);
                 mover.start();
-            } else if (map.getIsCurrentlyMoving() && !map.getIsDoingSolution()) {
+
+            } else if (map.getIsCurrentlyMoving()
+                    && !map.getIsDoingSolution()) {
                 mover.interrupt();
 
                 while (!mover.isInterrupted()) {
